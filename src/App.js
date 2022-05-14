a3a59d327156e6552eadf9af8f4e316fb7a467f9
gefgu <gefgu@hotmail.com>
2022-05-14 20:05:35 -0300

    Add routing between different page components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GameLevel from "./components/GameLevel";
+import Home from "./components/Home";
+import Leaderboard from "./components/Leaderboard";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<GameLevel />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
