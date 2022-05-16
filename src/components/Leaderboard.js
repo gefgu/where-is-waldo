d029209d286bd25d4724ba33fe8d8bbddea1e47b
gefgu <gefgu@hotmail.com>
2022-05-16 19:33:17 -0300

    Add leaderboard page html elements

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,7 +1,54 @@
+import LevelsDisplay from "./LevelsDisplay";
+
 const Leaderboard = () => {
   return (
-    <div>
-      <h1>Leaderboard Page!</h1>
+    <div className="leaderboard">
+      <h1>Leaderboard</h1>
+      <div className="buttons">
+        <button>Play This Level</button>
+        <button>Back To Home</button>
+      </div>
+      <LevelsDisplay />
+      <div className="data">
+        <table>
+          <thead>
+            <tr>
+              <th>NAME</th>
+              <th>TIME (SECONDS)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>User 1</td>
+              <td>1.245</td>
+            </tr>
+            <tr>
+              <td>User 2</td>
+              <td>1.245</td>
+            </tr>
+            <tr>
+              <td>User 3</td>
+              <td>1.245</td>
+            </tr>
+            <tr>
+              <td>User 4</td>
+              <td>1.245</td>
+            </tr>
+            <tr>
+              <td>User 5</td>
+              <td>1.245</td>
+            </tr>
+            <tr>
+              <td>User 6</td>
+              <td>1.245</td>
+            </tr>
+            <tr>
+              <td>User 7</td>
+              <td>1.245</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
