7e10e0423cf6f500e89c2f9727cec62fc1fe7d2f
gefgu <gefgu@hotmail.com>
2022-05-16 19:27:29 -0300

    Add refactor of levels display into separate component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,74 +1,11 @@
 import { Link } from "react-router-dom";
 import "../styles/home.css";
+import LevelsDisplay from "./LevelsDisplay";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="levels-display">
-        <div className="level">
-          <img src={require("../assets/level-1.jpg")} alt="level 1" />
-          <div className="description">
-            Level 1
-            <div className="icons">
-              <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
-              <img src={require("../assets/waldo.jpg")} alt="waldo" />
-              <img src={require("../assets/wizard.jpg")} alt="wizard" />
-            </div>
-          </div>
-        </div>
-        <div className="level">
-          <img src={require("../assets/level-2.jpg")} alt="level 2" />
-          <div className="description">
-            Level 2
-            <div className="icons">
-              <img src={require("../assets/waldo.jpg")} alt="waldo" />
-            </div>
-          </div>
-        </div>
-        <div className="level">
-          <img src={require("../assets/level-3.jpg")} alt="level 3" />
-          <div className="description">
-            Level 3
-            <div className="icons">
-              <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
-              <img src={require("../assets/waldo.jpg")} alt="waldo" />
-              <img src={require("../assets/wizard.jpg")} alt="wizard" />
-              <img src={require("../assets/wenda.jpg")} alt="wenda" />
-            </div>
-          </div>
-        </div>
-        <div className="level">
-          <img src={require("../assets/level-4.jpg")} alt="level 4" />
-          <div className="description">
-            Level 4
-            <div className="icons">
-              <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
-              <img src={require("../assets/waldo.jpg")} alt="waldo" />
-            </div>
-          </div>
-        </div>
-        <div className="level">
-          <img src={require("../assets/level-5.jpg")} alt="level 5" />
-          <div className="description">
-            Level 5
-            <div className="icons">
-              <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
-              <img src={require("../assets/waldo.jpg")} alt="waldo" />
-              <img src={require("../assets/wizard.jpg")} alt="wizard" />
-              <img src={require("../assets/wenda.jpg")} alt="wenda" />
-            </div>
-          </div>
-        </div>
-        <div className="level">
-          <img src={require("../assets/level-6.jpg")} alt="level 6" />
-          <div className="description">
-            Level 6
-            <div className="icons">
-              <img src={require("../assets/waldo.jpg")} alt="waldo" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <LevelsDisplay />
 
       <div className="leaderboard-card">
         <div>
---
+++ src/components/LevelsDisplay.js
@@ -0,0 +1,73 @@
+import "../styles/levelsDisplay.css";
+
+const LevelsDisplay = () => {
+  return (
+    <div className="levels-display">
+      <div className="level">
+        <img src={require("../assets/level-1.jpg")} alt="level 1" />
+        <div className="description">
+          Level 1
+          <div className="icons">
+            <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
+            <img src={require("../assets/waldo.jpg")} alt="waldo" />
+            <img src={require("../assets/wizard.jpg")} alt="wizard" />
+          </div>
+        </div>
+      </div>
+      <div className="level">
+        <img src={require("../assets/level-2.jpg")} alt="level 2" />
+        <div className="description">
+          Level 2
+          <div className="icons">
+            <img src={require("../assets/waldo.jpg")} alt="waldo" />
+          </div>
+        </div>
+      </div>
+      <div className="level">
+        <img src={require("../assets/level-3.jpg")} alt="level 3" />
+        <div className="description">
+          Level 3
+          <div className="icons">
+            <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
+            <img src={require("../assets/waldo.jpg")} alt="waldo" />
+            <img src={require("../assets/wizard.jpg")} alt="wizard" />
+            <img src={require("../assets/wenda.jpg")} alt="wenda" />
+          </div>
+        </div>
+      </div>
+      <div className="level">
+        <img src={require("../assets/level-4.jpg")} alt="level 4" />
+        <div className="description">
+          Level 4
+          <div className="icons">
+            <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
+            <img src={require("../assets/waldo.jpg")} alt="waldo" />
+          </div>
+        </div>
+      </div>
+      <div className="level">
+        <img src={require("../assets/level-5.jpg")} alt="level 5" />
+        <div className="description">
+          Level 5
+          <div className="icons">
+            <img src={require("../assets/odlaw.jpg")} alt="odlaw" />
+            <img src={require("../assets/waldo.jpg")} alt="waldo" />
+            <img src={require("../assets/wizard.jpg")} alt="wizard" />
+            <img src={require("../assets/wenda.jpg")} alt="wenda" />
+          </div>
+        </div>
+      </div>
+      <div className="level">
+        <img src={require("../assets/level-6.jpg")} alt="level 6" />
+        <div className="description">
+          Level 6
+          <div className="icons">
+            <img src={require("../assets/waldo.jpg")} alt="waldo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LevelsDisplay;
