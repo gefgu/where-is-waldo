af5485fe6252ca89bb622c759cf02cfd0de0f4f6
gefgu <gefgu@hotmail.com>
2022-05-17 19:49:19 -0300

    Add menu component with styling

---
+++ src/components/SelectionMenu.js
@@ -0,0 +1,11 @@
+import "../styles/selectionMenu.css";
+
+const SelectionMenu = () => {
+  return (
+    <div className="selection-menu">
+      <p>MENU</p>
+    </div>
+  );
+};
+
+export default SelectionMenu;
