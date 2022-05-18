435fb6a933bd30a1bc9f9605de141101ac8428de
gefgu <gefgu@hotmail.com>
2022-05-18 19:36:05 -0300

    Add initialization of app in firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GameLevel from "./components/GameLevel";
 import Heading from "./components/Heading";
 import Home from "./components/Home";
 import Leaderboard from "./components/Leaderboard";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
 import "./styles/app.css";
 
 function App() {
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+  });
+
   return (
     <BrowserRouter>
       <Heading />
