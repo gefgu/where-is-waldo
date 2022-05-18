fbf4c8df82a6579e3f6d5ce9df763d6ae78cfb1e
gefgu <gefgu@hotmail.com>
2022-05-17 21:03:22 -0300

    Add menu appearing on last click position on game

--- src/components/GameLevel.js
+++ src/components/GameLevel.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "../styles/gamelevel.css";
+import SelectionMenu from "./SelectionMenu";
 
 const GameLevel = () => {
+  const [menuX, setMenuX] = useState(0);
+  const [menuY, setMenuY] = useState(0);
+
   return (
     <div className="game-container">
       <div className="level-description">
@@ -11,7 +16,15 @@ const GameLevel = () => {
         </div>
         <button className="back">Return Home</button>
       </div>
-      <div className="game">
+      <div
+        className="game"
+        onClick={(e) => {
+          const bounds = e.target.getBoundingClientRect();
+          setMenuX(e.clientX - bounds.left);
+          setMenuY(e.clientY - bounds.top);
+        }}
+      >
+        <SelectionMenu x={menuX} y={menuY} />
         <img src={require("../assets/level-1.jpg")} alt="level 1" />
       </div>
     </div>
--- src/components/SelectionMenu.js
+++ src/components/SelectionMenu.js
@@ -1,8 +1,13 @@
 import "../styles/selectionMenu.css";
 
-const SelectionMenu = () => {
+const SelectionMenu = ({ x, y }) => {
+  const positionStyle = {
+    top: y,
+    left: x,
+  };
+
   return (
-    <div className="selection-menu">
+    <div className="selection-menu" style={positionStyle}>
       <p>MENU</p>
     </div>
   );
