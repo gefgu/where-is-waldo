730aa334d2ea6a0aa2d80ae8f8dbf9c8722a4213
gefgu <gefgu@hotmail.com>
2022-05-22 20:59:04 -0300

    Prevent selection menu from appearing when form is active

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,30 +1,34 @@
 import "../styles/userForm.css";
 
 const UserForm = ({ shouldDisplay = true }) => {
-  const positionStyle = {
-    top: "50%",
-    left: "50%",
-  };
+  const positionStyle = {};
 
   if (!shouldDisplay) positionStyle["display"] = "none";
   return (
-    <div className="user-form">
-      <h3>You finished in 12.345 seconds!</h3>
-      <form>
-        <div className="input-section">
-          <p>Enter your name to save your score on the global leaderboard.</p>
-          <label htmlFor="username">Username</label>
-          <input name="username" />
-        </div>
-        <div className="button-section">
-          <button type="button" className="cancel">
-            Cancel
-          </button>
-          <button type="submit" className="submit">
-            Submit Score
-          </button>
-        </div>
-      </form>
+    <div
+      className="overlay"
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <div className="user-form">
+        <h3>You finished in 12.345 seconds!</h3>
+        <form>
+          <div className="input-section">
+            <p>Enter your name to save your score on the global leaderboard.</p>
+            <label htmlFor="username">Username</label>
+            <input name="username" />
+          </div>
+          <div className="button-section">
+            <button type="button" className="cancel">
+              Cancel
+            </button>
+            <button type="submit" className="submit">
+              Submit Score
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
