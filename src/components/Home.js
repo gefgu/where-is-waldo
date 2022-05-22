b2070e96f87a5340bb97cb35453511c7afdab204
gefgu <gefgu@hotmail.com>
2022-05-22 16:17:03 -0300

    Refactor level display to accept click function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/home.css";
 import LevelsDisplay from "./LevelsDisplay";
 
 const Home = ({ levelsData }) => {
+  let navigate = useNavigate();
+
+  const levelClick = (level) => {
+    navigate(`/game/${level}`);
+  };
+
   return (
     <div className="home">
-      <LevelsDisplay levelsData={levelsData} />
+      <LevelsDisplay levelsData={levelsData} clickFunction={levelClick} />
 
       <div className="leaderboard-card">
         <div>
--- src/components/LevelsDisplay.js
+++ src/components/LevelsDisplay.js
@@ -1,34 +1,37 @@
 import "../styles/levelsDisplay.css";
-import { Link } from "react-router-dom";
 
-const LevelsDisplay = ({ levelsData }) => {
+const LevelsDisplay = ({ levelsData, clickFunction }) => {
   return (
     <div className="levels-display">
       {levelsData.map((levelData) => {
         const level = levelData.level;
         return (
-          <Link to={`/game/${level}`} key={level}>
-            <div className="level">
-              <img
-                src={require(`../assets/level-${level}.jpg`)}
-                alt={`Level ${level}`}
-              />
-              <div className="description">
-                {`Level ${level}`}
-                <div className="icons">
-                  {Object.keys(levelData.positions).map((character) => {
-                    return (
-                      <img
-                        key={character}
-                        src={require(`../assets/${character}.jpg`)}
-                        alt={character}
-                      />
-                    );
-                  })}
-                </div>
+          <div
+            className="level"
+            onClick={() => {
+              clickFunction(level);
+            }}
+            key={level}
+          >
+            <img
+              src={require(`../assets/level-${level}.jpg`)}
+              alt={`Level ${level}`}
+            />
+            <div className="description">
+              {`Level ${level}`}
+              <div className="icons">
+                {Object.keys(levelData.positions).map((character) => {
+                  return (
+                    <img
+                      key={character}
+                      src={require(`../assets/${character}.jpg`)}
+                      alt={character}
+                    />
+                  );
+                })}
               </div>
             </div>
-          </Link>
+          </div>
         );
       })}
     </div>
