044646b244277e8e379433711a30c6c4d428d6ed
Gustavo Santos <gefgu@hotmail.com>
2022-10-01 09:18:49 -0300

    feat: remove gitignore of firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMsfKMZZTB1oOQghAaPEheAT9oenyYq-A",
+
+  authDomain: "where-is-waldo-16811.firebaseapp.com",
+
+  projectId: "where-is-waldo-16811",
+
+  storageBucket: "where-is-waldo-16811.appspot.com",
+
+  messagingSenderId: "147203836719",
+
+  appId: "1:147203836719:web:9089394b5448c95f72efda",
+};
+
+export default firebaseConfig;
